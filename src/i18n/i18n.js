9dee73ec292a2c3a20c3e27c4035daf2ec897bf1
codemasher <smiley@chillerlan.net>
2021-02-09 03:45:00 +0100

    :octocat: +i18n worlds & upgrades

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -14,10 +14,18 @@ import {i18n_en} from './i18n-en.json';
 //import {i18n_fr} from './i18n-fr.json';
 //import {i18n_zh} from './i18n-zh.json';
 
-export const GW2MAP_I18N = {
+import {i18n_worlds} from './i18n-worlds.json';
+import {i18n_guild_upgrades} from './i18n-upgrades.json';
+
+const GW2MAP_I18N = {
 	de : i18n_de,
 	en : i18n_en,
 //	es : i18n_es,
 //	fr : i18n_fr,
 //	zh : i18n_zh,
 };
+
+const GW2_WVW_WORLDS = i18n_worlds;
+const GW2_GUILD_UPGRADES = i18n_guild_upgrades;
+
+export {GW2MAP_I18N, GW2_WVW_WORLDS, GW2_GUILD_UPGRADES};
